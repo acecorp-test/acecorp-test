dc6e1078a5dd332f2a30abde64e0ceda907b3e9b
acecorp-test <156060212+acecorp-test@users.noreply.github.com>
2024-01-09 03:16:38 -0500

    Add files via upload

---
+++ web/assets/js/srlhome.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('#slider1').anythingSlider(
+          {
+             delay:			7000,
+             animationTime:	2000,
+             buildArrows:	false,
+             resizeContents:	false,
+             easing:			'easeInOutExpo',
+         }
+    ); // add any non-default options here
+   });
